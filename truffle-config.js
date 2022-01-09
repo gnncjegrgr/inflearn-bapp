56007bbdf3a82d6eb32185ff02600b786b1d8db8
skystar234 <80614306+skystar234@users.noreply.github.com>
2022-01-09 20:37:33 +0900

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ const NETWORK_ID = "1001";
 const GASLIMIT = "20000000";
 const URL = `https://api.baobab.klaytn.net:8651`;
 const PRIVATE_KEY =
-  "0x8cafa33df8c1740720bc4815ce7c7cd61d18aaf396bb2a3da5e197f0c7b85aff";
+  "0x8cafa33df8c1740720bc4815ce7c7...";
 
 module.exports = {
   networks: {
